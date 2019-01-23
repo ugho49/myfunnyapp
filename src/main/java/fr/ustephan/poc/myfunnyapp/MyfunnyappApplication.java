b4de7a1e29e1d1e7d92c185c8e09779b6dbc4e99
Ugho STEPHAN <stephan.ugho@gmail.com>
2019-01-23 19:35:11 +0100

    first commit

---
+++ src/main/java/fr/ustephan/poc/myfunnyapp/MyfunnyappApplication.java
@@ -0,0 +1,14 @@
+package fr.ustephan.poc.myfunnyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfunnyappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfunnyappApplication.class, args);
+	}
+
+}
+
